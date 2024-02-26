7c9f5dcdc10d4c0cea7c106c5a35de5aaf87b54f
Keeghan McGarry <keeghan_m@live.com>
2024-02-26 16:09:59 +0000

    First untested attempt, saving as low laptop battery

--- main.ts
+++ main.ts
@@ -16,22 +16,30 @@ type TailcompStyles = {
     lg?:MediaStyles
 }
 
-export default function tc(classes: TailcompStyles) {
+export default function tc(classes: TailcompStyles): string {
     let classString = ''
-    /* ... Do stuff ... */
+    
+    // flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700
+
+    // Tailwind order:
+    // - all static classes from base
+    // - all static clases form sm/md/lg in that order
+    // - all state styles
+    // - all colour scheme styles
+
+    Object.keys(classes).forEach((mediaKey) => {
+        let tag = mediaKey === 'base' ? '' : `${mediaKey}:`
+        const mediaStyles = classes[mediaKey] as MediaStyles
+        Object.keys(mediaStyles).forEach((stateKey) => {
+            tag += stateKey === 'static' ? '' : `${stateKey}:`
+            const stateStyles = mediaStyles[stateKey] as StateStyles
+            Object.keys(stateStyles).forEach((colourKey) => {
+                tag += colourKey === 'dark' ? 'dark:' : ''
+                const colourStyles = stateStyles[colourKey] as string
+                classString += `${tag}${colourStyles} `
+            })
+        })
+    })
+
     return classString.trim()
 }
-
-tc({
-    base: {
-        static: "flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow",
-        hover: "bg-gray-100",
-        dark: {
-            static: "border-gray-700 bg-gray-800",
-            hover: "bg-gray-700"
-        }
-    },
-    md: {
-        static: "flex-row max-w-xl"
-    },
-})
