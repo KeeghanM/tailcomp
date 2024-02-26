d0c8ce3f4d957cf13ef0b2c2af8e97a20f7a53a9
Keeghan McGarry <keeghan_m@live.com>
2024-02-26 17:13:36 +0000

    Rework of generation - still not tested anything

--- main.ts
+++ main.ts
@@ -1,45 +1,41 @@
 type StateStyles = {
-    static: string
-    hover?: string
-    active?: string
-    focus?: string
-}
+    static: string;
+    hover?: string;
+    active?: string;
+    focus?: string;
+};
+
 type ColourStyles = {
-    dark?: StateStyles
-}
-type MediaStyles = StateStyles & ColourStyles
+    dark?: StateStyles;
+};
+
+type MediaStyles = StateStyles & ColourStyles;
 
 type TailcompStyles = {
-    base: MediaStyles
-    sm?:MediaStyles
-    md?:MediaStyles
-    lg?:MediaStyles
+    base: MediaStyles;
+    sm?: MediaStyles;
+    md?: MediaStyles;
+    lg?: MediaStyles;
+};
+
+function getMediaStyles(classes: TailcompStyles) {
+    return Object.keys(classes) as (keyof TailcompStyles)[]
+}
+
+function getStateStyles(styles: MediaStyles) {
+    return Object.keys(styles) as (keyof MediaStyles)[]
 }
 
 export default function tc(classes: TailcompStyles): string {
-    let classString = ''
-    
-    // flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700
+    let classString = '';
 
-    // Tailwind order:
-    // - all static classes from base
-    // - all static clases form sm/md/lg in that order
-    // - all state styles
-    // - all colour scheme styles
+    for(const mediaStyle of getMediaStyles(classes)) {
+        let prefix = mediaStyle === 'base' ? '' : `${mediaStyle}:`
+        for(const stateStyle of getStateStyles(classes[mediaStyle]!)) {
+            prefix += stateStyle === 'static' ? '' : `${stateStyle}:`
+            classString += stateStyle.split(' ').map(c => `${prefix}${c}`).join(' ')
+        }
+    }
 
-    Object.keys(classes).forEach((mediaKey) => {
-        let tag = mediaKey === 'base' ? '' : `${mediaKey}:`
-        const mediaStyles = classes[mediaKey] as MediaStyles
-        Object.keys(mediaStyles).forEach((stateKey) => {
-            tag += stateKey === 'static' ? '' : `${stateKey}:`
-            const stateStyles = mediaStyles[stateKey] as StateStyles
-            Object.keys(stateStyles).forEach((colourKey) => {
-                tag += colourKey === 'dark' ? 'dark:' : ''
-                const colourStyles = stateStyles[colourKey] as string
-                classString += `${tag}${colourStyles} `
-            })
-        })
-    })
-
-    return classString.trim()
+    return classString.trim();
 }
